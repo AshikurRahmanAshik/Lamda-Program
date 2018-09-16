640985b5a95ea18d9e48931a9ee38f05f64e4d0a
Ashik <ashik.cse.ru@gmail.com>
2018-09-16 16:06:12 +0600

    trying to learn lamda function in java

---
+++ src/main/MyGreeting.java
@@ -0,0 +1,5 @@
+package main;
+
+public interface MyGreeting {
+	String processName(String name);
+}
---
+++ src/main/NumericTest.java
@@ -0,0 +1,5 @@
+package main;
+
+public interface NumericTest {
+	boolean computerTest(int n);
+}
